a1aca997240af11b80c452ff7682d0cd80461bc7
Kamerynn Harrah <kamharrah8@gmail.com>
2015-07-01 16:11:49 -0400

    Implement functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,21 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function(){
+    var popped = storage[someInstance.size() - 1];
+    delete storage[someInstance.size() - 1];
+    return popped;
   };
 
   someInstance.size = function(){
+    var counter = 0;
+    _.each(storage, function() {
+      counter++;
+    });
+    return counter;
   };
 
   return someInstance;
