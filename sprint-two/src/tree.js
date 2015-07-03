98fa6dc47ee49799f3e94e9d4a711f4b37baae56
Kamerynn Harrah <kamharrah8@gmail.com>
2015-07-03 15:10:01 -0400

    Implement functional-shared tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,27 +2,44 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target){
+treeMethods.contains = function(target, node){
+  node = node || this;
+  var wasFound = false;
 
+  if (node.value === target) {
+    wasFound = true;
+    return wasFound;
+  }
+  // for loop through children
+  for (var i = 0; i < node.children.length; i++) {
+    // if contains with target, children[i]
+    wasFound = this.contains(target, node.children[i]);
+    if (wasFound) {
+      break;
+    }
+  }
+  // return wasFound
+  return wasFound;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+   addChild: O(1)
+   contains:
+      average: O(log(n))
+      worst: O(n)
  */
