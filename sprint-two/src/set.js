fb3e83a75e3e3a6068f1bafb0f8a6f75548d5e09
Kamerynn Harrah <kamharrah8@gmail.com>
2015-07-03 22:26:02 -0400

    Implement prototypal set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,34 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[this._keyGen(item)] = item;
 };
 
 setPrototype.contains = function(item){
+  return this._keyGen(item) in this._storage;
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[this._keyGen(item)];
 };
+
+// The purpose of keyGen is, right now, to store objects too
+// we will need to modify this to store functions
+setPrototype._keyGen = function(item) {
+  return JSON.stringify(item);
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+    add:      O(1)
+    contains: O(1)
+    remove:   O(1)
+ *   
  */
